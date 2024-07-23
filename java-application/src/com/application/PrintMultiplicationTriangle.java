b13e659e9c368f8cd1585075f8789361803af982
Nik Sun <nikswenxz@gmail.com>
2024-07-23 18:25:50 -0500

    demo for print multiplcation and triangle

---
+++ java-application/src/com/application/PrintMultiplicationTriangle.java
@@ -0,0 +1,18 @@
+package com.application;
+
+public class PrintMultiplicationTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " * " + i + " = " + j * i + '\t');
+            }
+            System.out.println( );
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            int spaces = 4 - i;
+            int stars = 2 * i - 1;
+            System.out.println(" ".repeat(spaces) + "*".repeat(stars));
+        }
+    }
+}
